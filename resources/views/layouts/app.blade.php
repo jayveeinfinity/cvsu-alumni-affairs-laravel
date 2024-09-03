a54c032f40f043b51e96f62c46daea53e417cb90
unknown <jayveeinfinity@gmail.com>
2024-09-03 20:43:48 +0800

    Landing page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,26 +2,18 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="x icon" type="img/png" href="/images/CvSU-logo-16x16.webp">
+    <link rel="x icon" type="img/png" href="/images/CvSU-logo-16x16.webp"><meta charset="UTF-8">
     <title>
       @yield('title') {{ config('app.name') }}
     </title>
-    <!-- Chart JS -->
-    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
-    <!-- Sweet Alert 2 -->
-    <link rel="stylesheet" href="{{asset('plugins/sweetalert2/sweetalert2.min.css')}}">
+    <!-- Tailwind CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
-    <!-- Chart JS -->
-    <link rel="stylesheet" href="{{asset('plugins/chart.js/Chart.css')}}">
-    <!-- Bootstrap Iconpicker CSS-->
-    <link rel="stylesheet" href="{{asset('plugins/fontawesome-iconpicker/css/bootstrap-iconpicker.css')}}"/>
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
   </head>
   <body style="background-color: #f3f4f6;">
     @include('partials.header')
